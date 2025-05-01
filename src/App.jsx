4ffd43720c037e3457ea455d13a4ab8c6d04d6f3
FENTON11 <fencurt99@gmail.com>
2025-05-01 16:14:54 +0300

    changed navbar name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/footer";
 import About from "./pages/About";
